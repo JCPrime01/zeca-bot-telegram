e1e52dc008e6f661a90f3024764fdcde4f0b8e15
JCPrime01 <vanzokiller@gmail.com>
2026-06-15 14:26:36 -0300

    chore: shareCode A7NXC495P9P -> U71RGYNYIR3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909&shareCode=A7NXC495P9P&afp4=bot&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909&shareCode=U71RGYNYIR3&afp4=bot&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
