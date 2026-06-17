b7bfe19feaeda4b7622e2b1451e83045a563e899
JCPrime01 <vanzokiller@gmail.com>
2026-06-16 22:40:25 -0300

    chore: shareCode 8IJNH5BHQB2 -> BPDWM1FW0HJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909&shareCode=8IJNH5BHQB2&afp4=bot&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909&shareCode=BPDWM1FW0HJ&afp4=bot&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
