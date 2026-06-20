c17225218c34fcf4b5d9a9297c653c594bef33b3
JCPrime01 <vanzokiller@gmail.com>
2026-06-19 22:56:03 -0300

    chore: shareCode D9SL0XOTRGY -> JTB9NM4YPJC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909&shareCode=D9SL0XOTRGY&afp4=bot&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909&shareCode=JTB9NM4YPJC&afp4=bot&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
