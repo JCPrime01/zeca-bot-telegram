90aaf4ae6dfb37518f8dacd52bb710006877f84a
JCPrime01 <vanzokiller@gmail.com>
2026-06-23 20:28:38 -0300

    chore: shareCode IGTWCF7O5L1 -> EX19OCHVQ98 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=IGTWCF7O5L1'
+    const shareCode = '&shareCode=EX19OCHVQ98'
     
     const afp = '&afp4=bot'
     
