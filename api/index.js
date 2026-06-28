79dc0b0607140a25ae4e1b9e448f81494ed70a66
JCPrime01 <vanzokiller@gmail.com>
2026-06-27 23:47:02 -0300

    chore: shareCode 36RURYR2UP7 -> JIIYC2NCT90 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=36RURYR2UP7'
+    const shareCode = '&shareCode=JIIYC2NCT90'
     
     const afp = '&afp4=bot'
     
