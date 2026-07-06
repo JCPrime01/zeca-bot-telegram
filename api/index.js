3e9877050909bca275834bd37bed43e4bfd980f0
JCPrime01 <vanzokiller@gmail.com>
2026-07-06 10:43:25 -0300

    chore: shareCode G5M6JWQG41K -> TMX22S29Q4C (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=G5M6JWQG41K'
+    const shareCode = '&shareCode=TMX22S29Q4C'
     
     const afp = '&afp4=bot'
     
