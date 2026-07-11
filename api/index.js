ce8daa281251e7fd7f650d50ee686f77e2f0b35f
JCPrime01 <vanzokiller@gmail.com>
2026-07-10 21:28:17 -0300

    chore: shareCode L3NIDD479W5 -> Y15Z0KIIZJP (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=L3NIDD479W5'
+    const shareCode = '&shareCode=Y15Z0KIIZJP'
     
     const afp = '&afp4=bot'
     
