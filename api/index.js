391882470b183ced8fa56b89a23c216e3f1012e4
JCPrime01 <vanzokiller@gmail.com>
2026-07-16 07:17:39 -0300

    chore: shareCode USNV4CC2HVQ -> 1HW1G07V391 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=USNV4CC2HVQ'
+    const shareCode = '&shareCode=1HW1G07V391'
     
     const afp = '&afp4=bot'
     
