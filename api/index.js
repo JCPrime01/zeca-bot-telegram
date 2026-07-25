714f35e13841535ab6bcdf6fd48e08d3d37b8b71
JCPrime01 <vanzokiller@gmail.com>
2026-07-25 19:03:23 -0300

    chore: shareCode MVGBCOMLEOW -> W3GQM794E2N (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=MVGBCOMLEOW'
+    const shareCode = '&shareCode=W3GQM794E2N'
     
     const afp = '&afp4=bot'
     
