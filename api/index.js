2030d02b977a9f4049ec305b78dcead6f7e5f15d
JCPrime01 <vanzokiller@gmail.com>
2026-07-27 07:18:37 -0300

    chore: shareCode W3GQM794E2N -> 64F75G7V12P (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=W3GQM794E2N'
+    const shareCode = '&shareCode=64F75G7V12P'
     
     const afp = '&afp4=bot'
     
