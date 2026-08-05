7aefead9f7fd99bc27886bd5c60475a89cbc53f4
JCPrime01 <vanzokiller@gmail.com>
2026-08-05 08:21:34 -0300

    chore: shareCode GTELG1Q4US9 -> 5BK7E21GS53 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=GTELG1Q4US9'
+    const shareCode = '&shareCode=5BK7E21GS53'
     
     const afp = '&afp4=bot'
     
