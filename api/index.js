d05edb23f36ee955f1a08c4dba78de6e2e0de653
JCPrime01 <vanzokiller@gmail.com>
2026-08-05 21:40:13 -0300

    chore: shareCode KTNEHP3CKV2 -> VG41PMM6YL3 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=KTNEHP3CKV2'
+    const shareCode = '&shareCode=VG41PMM6YL3'
     
     const afp = '&afp4=bot'
     
