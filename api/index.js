8ab922bad51d9b825023e85dbeea9a1d5f6a8285
JCPrime01 <vanzokiller@gmail.com>
2026-08-12 09:29:14 -0300

    chore: shareCode HIHBFVYYB5F -> PA5GGEBYOUE (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=HIHBFVYYB5F'
+    const shareCode = '&shareCode=PA5GGEBYOUE'
     
     const afp = '&afp4=bot'
     
