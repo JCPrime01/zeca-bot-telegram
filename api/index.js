c9effceb4fbad4b066bbdf7b5bbe5f9f937438a6
JCPrime01 <vanzokiller@gmail.com>
2026-08-17 21:04:34 -0300

    chore: shareCode AXQA55EN24I -> CP48WBPRO8G (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=AXQA55EN24I'
+    const shareCode = '&shareCode=CP48WBPRO8G'
     
     const afp = '&afp4=bot'
     
