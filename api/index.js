62748f7ffa3a1b9a80eae54785d5ab39fb8533e1
JCPrime01 <vanzokiller@gmail.com>
2026-08-19 23:39:25 -0300

    chore: shareCode GP38E5X5PE7 -> YLY4UJRTT65 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=GP38E5X5PE7'
+    const shareCode = '&shareCode=YLY4UJRTT65'
     
     const afp = '&afp4=bot'
     
