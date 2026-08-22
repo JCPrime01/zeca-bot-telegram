7d9985a57f4ad878150970adc7d93b690748c903
JCPrime01 <vanzokiller@gmail.com>
2026-08-22 12:42:31 -0300

    chore: shareCode MAADIZOVBD3 -> YDMDSFKPOVR (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=MAADIZOVBD3'
+    const shareCode = '&shareCode=YDMDSFKPOVR'
     
     const afp = '&afp4=bot'
     
