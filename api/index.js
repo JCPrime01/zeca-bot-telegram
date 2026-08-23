872f3fe083d34d8645eed859a6763791671265a9
JCPrime01 <vanzokiller@gmail.com>
2026-08-23 16:16:58 -0300

    chore: shareCode SQ3IKP4EIC4 -> ZRLVDO3AV4O (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=SQ3IKP4EIC4'
+    const shareCode = '&shareCode=ZRLVDO3AV4O'
     
     const afp = '&afp4=bot'
     
