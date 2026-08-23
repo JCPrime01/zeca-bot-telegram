7922a343bbdb92928989ea8ec27266ceab9012b6
JCPrime01 <vanzokiller@gmail.com>
2026-08-23 10:43:52 -0300

    chore: shareCode YDMDSFKPOVR -> SQ3IKP4EIC4 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=YDMDSFKPOVR'
+    const shareCode = '&shareCode=SQ3IKP4EIC4'
     
     const afp = '&afp4=bot'
     
