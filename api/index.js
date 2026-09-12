88875be37878f0dcbf98a848e0b571fbdf06e1f0
Devgrupojc <devgrupojc@gmail.com>
2026-09-12 08:37:50 -0300

    chore: shareCode EB6A4ZGUA4F -> UMOIYLHQV5E (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=EB6A4ZGUA4F'
+    const shareCode = '&shareCode=UMOIYLHQV5E'
     
     const afp = '&afp4=bot'
     
