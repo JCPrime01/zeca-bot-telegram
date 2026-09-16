1c4f33bfdb74dc0c80c8391ea67bf97530c59f52
Devgrupojc <devgrupojc@gmail.com>
2026-09-16 07:42:35 -0300

    chore: shareCode OKQRNA6RBNI -> EPDD1NU5XFJ (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=OKQRNA6RBNI'
+    const shareCode = '&shareCode=EPDD1NU5XFJ'
     
     const afp = '&afp4=bot'
     
