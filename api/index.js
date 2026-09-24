83c175891bc877476dc71244d997a3c8a26531ee
Devgrupojc <devgrupojc@gmail.com>
2026-09-23 23:07:44 -0300

    chore: shareCode 6VSSS26SCGZ -> HOY9GODPHGN (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=29909'
 
-    const shareCode = '&shareCode=6VSSS26SCGZ'
+    const shareCode = '&shareCode=HOY9GODPHGN'
     
     const afp = '&afp4=bot'
     
